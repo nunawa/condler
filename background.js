14899e814c9b811276f401f107f367e1b1170079
nunawa <71294849+nunawa@users.noreply.github.com>
2021-07-23 14:38:02 +0900

    Improve handling of local storage

--- background.js
+++ background.js
@@ -1,29 +1,28 @@
 // 要素非表示設定保存用
-// アドオンの要素数
+// 要素数取得
 $.get(browser.runtime.getURL("search-options-dom.html"), function(data) {
-    let i = 0;
+    let numel = 0;
     while (true) {
-        if (!$(data).find("[id='ele"+i+"']").length && !$(data).siblings("[id='ele"+i+"']").length) {
+        if (!$(data).find("[id='ele"+numel+"']").length && !$(data).siblings("[id='ele"+numel+"']").length) {
             break;
         }
-        i++;
+        numel++;
     }
-    //console.log("numel: " + i);
-    browser.storage.local.set({numel: i});
-});
+    console.log("numel: " + numel);
+    browser.storage.local.get("numel").then(function(result) {
+        // 保存されている要素数と今取得した要素数が違う場合
+        if (result.numel != numel) {
+            // result.numelが空の時もここの処理入る
+            console.log("numel is empty or mismatch prev numel");
 
-// 表示非表示設定
-browser.storage.local.get("ele0").then(function(result) {
-    if (!Object.keys(result).length) {
-        //console.log("result is empty");
-
-        let data = {};
-        browser.storage.local.get("numel").then(function(result) {
-            for (let i = 0; i < result.numel; i++) {
+            // 表示非表示データをリセット
+            let data = {};
+            for (let i = 0; i < numel; i++) {
                 const key = "ele" + i;
                 data[key] = true;
             }
             browser.storage.local.set(data);
-        });
-    }
+        }
+    });
+    browser.storage.local.set({numel: numel});
 });
